a190d8b560321d58977c4a5b6e58ae0f9103932e
Adam Moszczynski <adam.1.moszczynski@nokia.com>
2022-03-22 21:53:55 +0100

    final version

--- pong.py
+++ pong.py
@@ -4,12 +4,14 @@ from pgzero.actor import Actor
 from random import randint, choice
 
 class Palette():
-    def __init__(self, palette, posicion):
+    def __init__(self, palette, posicion, width=140, ball_width=10):
         """Palette i jej właściwości"""
         self.palette = palette
         self.palette.x = posicion[0]
         self.palette.y = posicion[1]
         self.palette.speed = 5
+        self.palette.pcenter = width//2
+        self.palette.ball_width = ball_width
 
     def drawing(self):
         self.palette.draw()
@@ -17,12 +19,28 @@ class Palette():
     def move(self, direction):
         if direction == "left":
             self.palette.x -= self.palette.speed
-            if self.palette.x < 70:
-                self.palette.x = 75
+            if self.palette.x < self.palette.pcenter:
+                self.palette.x = self.palette.pcenter + 5
         elif direction == "right":
             self.palette.x += self.palette.speed
-            if self.palette.x > (WIDTH - 70):
-                self.palette.x = WIDTH - 75
+            if self.palette.x > (WIDTH - self.palette.pcenter):
+                self.palette.x = WIDTH - self.palette.pcenter + 5
+
+    def bounce(self):
+        if (
+                self.palette.distance_to(ball) > self.palette.pcenter + self.palette.ball_width)\
+                :
+            return False
+
+        if abs(self.palette.y - ball.y) > self.palette.ball_width * 2:
+            return False
+
+        if self.palette.x > ball.x and ball.direction_x == "left":
+            ball.direction_x = "right"
+        elif self.palette.x > ball.x and ball.direction_x == "right":
+            ball.direction_x = "left"
+
+        return True
 
 def update_ball_position():
     #aktualna pozycja na osi x
@@ -39,15 +57,28 @@ def update_ball_position():
 
     #sprawdzanie czy piłeczka odbije się od krawędzi okna
     if ball.x < 5:
-        ball.direction_x == "right"
-    elif ball.x < WIDTH - 5:
-        ball.direction_x == "left"
+        ball.direction_x = "right"
+    elif ball.x > WIDTH - 5:
+        ball.direction_x = "left"
 
     #sprawdzanie czy ktoś wygrał
     if ball.y < 5:
         ball.winner = "gracz_b"
     elif ball.y > HEIGHT - 5:
         ball.winner = "gracz a"
+
+def check_bounce():
+    if palette_a.bounce():
+        ball.direction_y = "down"
+    if palette_b.bounce():
+        ball.direction_y = "up"
+
+def check_winner():
+    if ball.winner:
+        winner_txt = f"Fuck the winner: {ball.winner}"
+        screen.draw.text(
+            winner_txt, (WIDTH//3 , HEIGHT // 2), color="yellow", fontsize=30
+        )
 
 #start programu
 WIDTH = 1280
@@ -93,6 +124,7 @@ def update_palettes():
 def update():
     update_ball_position()
     update_palettes()
+    check_bounce()
 
 
 def draw():
@@ -101,5 +133,6 @@ def draw():
     palette_a.drawing()
     palette_b.drawing()
     ball.draw()
+    check_winner()
 
 pgzrun.go()
